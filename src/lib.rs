b6fdb0235c8d010ce3918857d84958c5b0daa3ae
dennie-seth <dennie.seth@hotmail.com>
2025-11-30 08:34:38 +0100

    upd: client supports file sending

--- src/lib.rs
+++ src/lib.rs
@@ -1,22 +1,39 @@
 ﻿use pyo3::prelude::*;
 use pyo3::wrap_pyfunction;
 use pyo3_asyncio::tokio::init_multi_thread_once;
-use crate::net::client::client_connect;
+use crate::net::client::{client_connect, client_send, PyBrokerClient};
 mod net;
 
 #[pyfunction]
-fn connect(py: Python<'_>, url: String) -> PyResult<&PyAny> {
+unsafe fn connect(py: Python<'_>, url: String) -> PyResult<&PyAny> {
     pyo3_asyncio::tokio::future_into_py(py, async move {
         match client_connect(url).await {
-            Ok(_) => Ok(()),
+            Ok(client) => {
+                Python::with_gil(|py| {
+                    let py_client = PyBrokerClient {
+                        client: client.clone(),
+                    };
+                    Py::new(py, py_client).map_err(Into::into)
+                })
+            }
             Err(_) => Err(pyo3::exceptions::PyRuntimeError::new_err("connection failed")),
         }
     })
 }
-
+#[pyfunction]
+unsafe fn send<'py>(py: Python<'py>, client: &PyBrokerClient, path: String) -> PyResult<&'py PyAny> {
+    let client = client.client.clone();
+    pyo3_asyncio::tokio::future_into_py(py, async move {
+        match client_send(client, &path).await {
+            Ok(_) => Ok(()),
+            Err(err) => Err(pyo3::exceptions::PyRuntimeError::new_err(format!("failed to send, {err}"))),
+        }
+    })
+}
 #[pymodule]
 fn data_broker_client(py: Python<'_>, m: &PyModule) -> PyResult<()> {
     pyo3_asyncio::tokio::init_multi_thread_once();
     m.add_function(wrap_pyfunction!(connect, m)?)?;
+    m.add_function(wrap_pyfunction!(send, m)?)?;
     Ok(())
 }
