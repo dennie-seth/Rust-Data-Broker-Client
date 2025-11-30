2d60192f8e3a45b3d983dbea5b242aedacde0b60
dennie-seth <dennie.seth@hotmail.com>
2025-11-30 09:24:34 +0100

    upd: changed tokio runtime creation

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,18 @@
-﻿use pyo3::prelude::*;
+﻿use std::sync::OnceLock;
+use pyo3::prelude::*;
 use pyo3::wrap_pyfunction;
-use pyo3_asyncio::tokio::init_multi_thread_once;
+use pyo3_asyncio::tokio::init_with_runtime;
+use tokio::runtime::Runtime;
 use crate::net::client::{client_connect, client_send, PyBrokerClient};
 mod net;
 
+static RUNTIME: OnceLock<Runtime> = OnceLock::new();
+
+fn get_runtime() -> &'static Runtime {
+    RUNTIME.get_or_init(|| {
+        Runtime::new().expect("Failed to create Tokio runtime")
+    })
+}
 #[pyfunction]
 unsafe fn connect(py: Python<'_>, url: String) -> PyResult<&PyAny> {
     pyo3_asyncio::tokio::future_into_py(py, async move {
@@ -32,7 +41,8 @@ unsafe fn send<'py>(py: Python<'py>, client: &PyBrokerClient, path: String) -> P
 }
 #[pymodule]
 fn data_broker_client(py: Python<'_>, m: &PyModule) -> PyResult<()> {
-    pyo3_asyncio::tokio::init_multi_thread_once();
+    let rt: &'static Runtime = get_runtime();
+    let _ = init_with_runtime(rt);
     m.add_function(wrap_pyfunction!(connect, m)?)?;
     m.add_function(wrap_pyfunction!(send, m)?)?;
     Ok(())
